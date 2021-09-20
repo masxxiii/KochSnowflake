e6c03f5d37b15550363dcfe3b843defacd22b70c
maszafar <57627236+maszafar@users.noreply.github.com>
2021-09-20 19:30:21 +0700

    Add files via upload

---
+++ Practical2_KochSnowflake/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Practical2_KochSnowflake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(201, 580);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DRAW";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(720, 675);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(750, 750);
+            this.MinimumSize = new System.Drawing.Size(750, 750);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Koch Snowflake";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Practical2_KochSnowflake/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical2_KochSnowflake
+{
+    public partial class Form1 : Form
+    {
+        static Graphics obj;
+        static Pen penBlue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create a pen and a drawing area
+            penBlue = new Pen(Color.DarkBlue, 2);
+            obj = CreateGraphics();
+            obj.Clear(Color.White);
+            
+            //simple triangle with fixed points
+            var point1 = new PointF(200, 200);
+            var point2 = new PointF(500, 200);
+            var point3 = new PointF(350, 400);
+            obj.DrawLine(penBlue, point1, point2);
+            obj.DrawLine(penBlue, point2, point3);
+            obj.DrawLine(penBlue, point3, point1);
+
+            //calling the function
+            FractalKoh(point1, point2, point3, 5);
+            FractalKoh(point2, point3, point1, 5);
+            FractalKoh(point3, point1, point2, 5);
+        }
+
+        static int FractalKoh(PointF p1, PointF p2, PointF p3, int iter)
+        {
+            if (iter > 0)
+            {
+                var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
+                var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+
+                var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
+                var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
+
+                obj.DrawLine(penBlue, p4, pn);
+                obj.DrawLine(penBlue, p5, pn);
+                obj.DrawLine(penBlue, p4, p5);
+
+                FractalKoh(p4, pn, p5, iter - 1);
+                FractalKoh(pn, p5, p4, iter - 1);
+                FractalKoh(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iter - 1);
+                FractalKoh(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iter - 1);
+            }
+            return iter;
+        }
+    }
+}
